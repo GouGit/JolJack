ca4e318687c543cd80dd07dd19170aff830e5ed7
sweets125 <an060875@naver.com>
2019-06-06 16:27:36 +0900

    -FadeUI 스크립트 추가 -SceneLoader 스크립트 추가 -Title 씬 추가

---
+++ Gun/Assets/Code/SceneLoader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public static void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+
+    public static string GetNowSceneName()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    public static int GetNowSceneIndex()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public static void ReloadScene()
+    {
+        LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadSceneWithFade(string name)
+    {
+        FadeUI fadePanel = FindObjectOfType<FadeUI>();
+        fadePanel.m_OnFadeInEnd += (obj) =>
+        {
+            LoadScene(name);
+        };
+        fadePanel.FadeIn();
+    }
+
+    public void LoadSceneWithFade(int index)
+    {
+        FadeUI fadePanel = FindObjectOfType<FadeUI>();
+        fadePanel.m_OnFadeInEnd += (obj) =>
+        {
+            LoadScene(index);
+        };
+        fadePanel.FadeIn();
+    }
+
+    public void ChangeSceneToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ChangeSceneToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ChangeSceneToWaveGame()
+    {
+        SceneManager.LoadScene("Game_Wave");
+    }
+}
