ca881b939234468e05579e0a4e4306a01a231334
dat0613 <dat0613@naver.com>
2019-06-11 21:35:05 +0900

    간단하게 게임에 넣을정도는 완성
    사용법은 주석과 ToolTip을 보면 충분히 숙지할 수 있다고 생각함.

---
+++ Gun/Assets/Code/MapTree/Spot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spot : MonoBehaviour
+{
+    public LineRenderer liner;
+    [Tooltip("이 지점로 오는 길은 되돌아 갈 수 없는 길이 됩니다.")]
+    public bool isOneWay;               // true 일시 이spot으로 오는 길은 뒤로 돌아갈 수 없는 길이 됩니다.
+    [Tooltip("이 지점에서 길을 이을 다음 지점 입니다.")]
+    public List<Spot> nextRoutes;       // 다음으로 이어질 spot 입니다.
+
+    public void test()
+    {
+        Debug.Log("와!!!");
+    }
+
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        liner.positionCount = nextRoutes.Count * 2;
+        
+        int count = nextRoutes.Count;
+
+        for(int i = 0; i < count; i++)
+        {
+            int offset = (i + 1) * 2 - 1;
+            liner.SetPosition(offset - 1, transform.position);
+            liner.SetPosition(offset, nextRoutes[i].transform.position);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Gun/Assets/Code/MapTree/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Traveler traveler;
+
+    public void Change()
+    {
+        Debug.Log("spot이 변경 되었습니다.");
+    }
+
+    public void GoStart()
+    {
+        Debug.Log("움직이기 시작합니다");
+    }
+
+    public void GoEnd()
+    {
+        Spot nowSpot = traveler.nowSpot;
+        if(nowSpot.nextRoutes.Count > 0)
+        {
+            Debug.Log("움직임이 끝나 다음으로 이동합니다.");
+            traveler.ChangeSpot(nowSpot.nextRoutes[0]);
+        }
+        else
+        {
+            Debug.Log("더 이상 갈수 없습니다.");
+        }
+    }
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Start()
+    {
+        traveler.ChangeSpot(traveler.nowSpot.nextRoutes[0]);
+    }
+}
