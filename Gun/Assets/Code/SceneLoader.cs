341ccdfa4ceeb2b0a11db3d828f940f254aaa301
sweets125 <an060875@naver.com>
2019-06-17 11:13:39 +0900

    -씬 로더 수정 (충돌 일어나므로 저장소꺼 사용 예정)

--- Gun/Assets/Code/SceneLoader.cs
+++ Gun/Assets/Code/SceneLoader.cs
@@ -53,7 +53,7 @@ public class SceneLoader : MonoBehaviour
 
     public void ChangeSceneToMenu()
     {
-        SceneManager.LoadScene("Menu");
+        LoadScene("Menu");
     }
 
     public void ChangeSceneToGame()
